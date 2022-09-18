f4d2d83a2995884402da75f7236267457edad718
manthanparekh <manthan.parekh@outlook.com>
2022-09-18 08:21:38 -0400

    change main

--- LinearSearch.java
+++ LinearSearch.java
@@ -8,7 +8,9 @@ public class LinearSearch {
         return -1;
     }
 
-    public static void main(String a[]) {
+    public static void main(String[] args)
+
+    {
         int[] a1 = { 10, 20, 30, 50, 70, 90 };
         int key = 50;
         System.out.println(key + " is found at index: " + linearSearch(a1, key));
